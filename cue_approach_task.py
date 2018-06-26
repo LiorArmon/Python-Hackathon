81e8c1af82489acb9e0b8544998a41ac13a711da
liorarmon <liorarmon@gmail.com>
2018-06-26 19:23:35 +0200

    added some pseudo code

--- cue_approach_task.py
+++ cue_approach_task.py
@@ -2,6 +2,11 @@ import psychopy.event
 import psychopy.core
 import psychopy.visual
 import psychopy.gui
+import psychopy.sound
+import os
+
+images = os.listdir("Images")
+#open a file the contains DataFrame with all the stimuli and the "play sound" boolean
 
 # subject info window
 gui = psychopy.gui.Dlg()
@@ -14,6 +19,7 @@ gui.show()
 subj_id = gui.data[0]
 subj_age = gui.data[1]
 subj_gender = gui.data[2]
+#save as a metadata to the Results DataFrame
 
 
 # window
@@ -30,21 +36,34 @@ fixation = psychopy.visual.ShapeStim(win,
     lineColor="white"
 ) 
 
+#add a loop that runs on a  range that equals to the number of trials set in the settings doc
 # image
-img = psychopy.visual.ImageStim(
-        win=win,
-        image="Images\snickers.jpg",
-        units="pix",
-        size=[400,400])
+for image in images: #repeat for each image in the folder
+    img = psychopy.visual.ImageStim(
+            win=win,
+            image=os.path.normpath("Images/"+image), #opens a picture from Images folder
+            units="pix",
+            size=[400,400])
+    
+    #draw fixation cross
+    fixation.draw()
+    win.flip()
+    psychopy.core.wait(0.980) #wait 1 sec
+    
+    # initializing the clock for RT
+    clock = psychopy.core.Clock()
+    
+    #draw image
+    img.draw()
+    win.flip()
+    psychopy.core.wait(0.7)
 
-#draw fixation cross
-fixation.draw()
-win.flip()
-psychopy.core.wait(0.980) #wait 1 sec
-
-#draw image
-img.draw()
-win.flip()
-psychopy.event.waitKeys() #wait for user to press a button
+# need to add a condition - if in the stimulus data frame, boolean is True do this:
+    #sound
+    s=psychopy.sound.Sound(value="G", secs=0.1) #creates a sound
+    s.play() #plays the sound
+    keys = psychopy.event.getKeys(keyList = ["b"], timeStamped=clock) #records the RT for a button press
+    psychopy.core.wait(0.3)
+# should append the results from the keys list to a Results DataFrame with attributes "stimulus, RT"
 
 win.close()
